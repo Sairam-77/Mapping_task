c529311a6822975a1a42abaa43cba0d6dd95d4a5
Sairam-77 <sairam.babu@kprinfo.com>
2022-09-14 17:11:52 +0530

    .

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
-import {useParams} from "react-router-dom";
+import React, { useContext} from 'react'
+
 import { Context } from '../App';
 import { useNavigate } from 'react-router-dom';
 
